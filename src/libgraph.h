df91934deedfb5f57965cb4c289a07f71e66e3c1
aldidoanta <aldi_ginting@live.com>
2014-05-21 18:31:56 +0700

    some fixes according to cgetpixel()

--- src/libgraph.h
+++ src/libgraph.h
@@ -12,8 +12,8 @@
 /** Graphic Primitive **/
 
 void init_graph();
-void paintpix(int x, int y, int color); //getpixel for cartesian coordinate system
-int cgetpixel(int x, int y);
+void paintpix(int x, int y, int color); //putpixel for cartesian coordinate system
+int cgetpixel(int x, int y); //getpixel for cartesian coordinate system
 
 /** Graphics Algorithms **/
 
