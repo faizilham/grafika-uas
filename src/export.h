cbec75fa0892ab56e51d054471e172c331c7cb40
unknown <adilelfahmi@gmail.com>
2014-05-23 00:26:07 +0700

    export image

---
+++ src/export.h
@@ -0,0 +1,11 @@
+#ifndef EXPORT_H
+#define EXPORT_H
+
+#include <windows.h>
+#include <stdio.h>
+#include "libgraph.h"
+
+unsigned char converttorgb(int pixel, int comp);
+void export_canvas();
+
+#endif
